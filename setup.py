08e40122492ad3eb6c0e7cda70f4e01de0ad1ceb
Claudio <claudio.rizzo.90@gmail.com>
2017-10-09 11:42:22 +0100

    setup packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='android-python-analysis',
+      version='1.0dev',
+      description='Python Distribution Utilities',
+      author='Claudio Rizzo',
+      author_email='claudio.rizzo.90@gmail.com',
+      url='https://github.com/ClaudioRizzo/android-python-analysis',
+      packages=['android',],
+     )
